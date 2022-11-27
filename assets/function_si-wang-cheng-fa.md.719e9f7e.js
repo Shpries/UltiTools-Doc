90cb7f92fb030bfcebcb4e404590e3e500abad27
qianmo2233 <qianmo2233@users.noreply.github.com>
2022-11-27 02:22:06 +0000

    deploy: 28abe03c573b0670ce40fa6f05c4571ecfd3b644

---
+++ assets/function_si-wang-cheng-fa.md.719e9f7e.js
@@ -0,0 +1,39 @@
+import{_ as s,o as n,c as a,a as l}from"./app.adfe9b0b.js";const p="/assets/\u6B7B\u4EA1\u60E9\u7F5A.jpg",_=JSON.parse('{"title":"\u6B7B\u4EA1\u60E9\u7F5A","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u6548\u679C\u56FE","slug":"\u6548\u679C\u56FE"},{"level":2,"title":"\u4F7F\u7528\u6559\u7A0B","slug":"\u4F7F\u7528\u6559\u7A0B"}],"relativePath":"function/si-wang-cheng-fa.md","lastUpdated":1669515692000}'),o={name:"function/si-wang-cheng-fa.md"},e=l('<h1 id="\u6B7B\u4EA1\u60E9\u7F5A" tabindex="-1">\u6B7B\u4EA1\u60E9\u7F5A <a class="header-anchor" href="#\u6B7B\u4EA1\u60E9\u7F5A" aria-hidden="true">#</a></h1><p>\u670D\u52A1\u5668\u5F00\u542F\u4E86\u7269\u54C1\u4E0D\u6389\u843D\uFF0C\u73A9\u5BB6\u90FD\u4E00\u4E2A\u4E2A\u5929\u4E0D\u6015\u5730\u4E0D\u6015\uFF1F\u5931\u53BB\u4E86\u539F\u7248\u7684\u7D27\u5F20\u523A\u6FC0\u600E\u4E48\u884C\uFF1F</p><p>UltiTools\u63D0\u4F9B\u6B7B\u4EA1\u60E9\u7F5A\uFF0C\u53EF\u8BBE\u7F6E\u6389\u843D\u91D1\u5E01\uFF0C\u968F\u673A\u6389\u843D\u7269\u54C1\uFF0C\u652F\u6301\u6B7B\u4EA1\u6267\u884C\u540E\u53F0\u6307\u4EE4\uFF0C\u652F\u6301\u9009\u62E9\u4E16\u754C\u5F00\u542F\u91D1\u5E01\u6216\u8005\u7269\u54C1\u6389\u843D\u3002</p><h2 id="\u6548\u679C\u56FE" tabindex="-1">\u6548\u679C\u56FE <a class="header-anchor" href="#\u6548\u679C\u56FE" aria-hidden="true">#</a></h2><p><img src="'+p+`" alt=""></p><h2 id="\u4F7F\u7528\u6559\u7A0B" tabindex="-1">\u4F7F\u7528\u6559\u7A0B <a class="header-anchor" href="#\u4F7F\u7528\u6559\u7A0B" aria-hidden="true">#</a></h2><p>\u6253\u5F00\u6587\u4EF6Ultitools\\death.yml</p><div class="language-yaml"><button class="copy"></button><span class="lang">yaml</span><pre><code><span class="line"><span style="color:#676E95;"># \u914D\u7F6E\u6587\u4EF6\u7248\u672C\uFF0C\u8BF7\u52FF\u4FEE\u6539</span></span>
+<span class="line"><span style="color:#F07178;">config_version</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> </span><span style="color:#F78C6C;">1.0</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#676E95;"># \u662F\u5426\u5F00\u542F\u7269\u54C1\u6389\u843D</span></span>
+<span class="line"><span style="color:#F07178;">enable_item_drop</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> </span><span style="color:#FF9CAC;">false</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#676E95;"># \u662F\u5426\u5F00\u542F\u91D1\u5E01\u6389\u843D</span></span>
+<span class="line"><span style="color:#F07178;">enable_money_drop</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> </span><span style="color:#FF9CAC;">true</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#676E95;"># \u662F\u5426\u5F00\u542F\u6B7B\u4EA1\u6267\u884C\u540E\u53F0\u547D\u4EE4</span></span>
+<span class="line"><span style="color:#F07178;">enable_punish_commands</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> </span><span style="color:#FF9CAC;">true</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#676E95;"># \u6B7B\u4EA1\u540E\u6389\u843D\u7684\u91D1\u5E01\u6570</span></span>
+<span class="line"><span style="color:#F07178;">money_dropped_ondeath</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> </span><span style="color:#F78C6C;">100</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#676E95;"># \u6B7B\u4EA1\u540E\u968F\u673A\u6389\u843D\u7684\u7269\u54C1\u6570</span></span>
+<span class="line"><span style="color:#F07178;">item_dropped_ondeath</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> </span><span style="color:#F78C6C;">3</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#676E95;"># \u6B7B\u4EA1\u540E\u540E\u53F0\u6267\u884C\u7684\u6307\u4EE4\uFF08{PLAYER}\u6307\u4EE3\u73A9\u5BB6\u5360\u4F4D\u7B26\uFF09</span></span>
+<span class="line"><span style="color:#F07178;">punish_command</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> </span><span style="color:#89DDFF;">[]</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#676E95;"># \u5F00\u542F\u6B7B\u4EA1\u968F\u673A\u7269\u54C1\u6389\u843D\u7684\u4E16\u754C</span></span>
+<span class="line"><span style="color:#F07178;">worlds_enabled_item_drop</span><span style="color:#89DDFF;">:</span></span>
+<span class="line"><span style="color:#A6ACCD;">  </span><span style="color:#89DDFF;">-</span><span style="color:#A6ACCD;"> </span><span style="color:#C3E88D;">world</span></span>
+<span class="line"><span style="color:#A6ACCD;">  </span><span style="color:#89DDFF;">-</span><span style="color:#A6ACCD;"> </span><span style="color:#C3E88D;">world_nether</span></span>
+<span class="line"><span style="color:#A6ACCD;">  </span><span style="color:#89DDFF;">-</span><span style="color:#A6ACCD;"> </span><span style="color:#C3E88D;">world_the_end</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#676E95;"># \u5F00\u542F\u6B7B\u4EA1\u540E\u91D1\u5E01\u6389\u843D\u7684\u4E16\u754C</span></span>
+<span class="line"><span style="color:#F07178;">worlds_enabled_money_drop</span><span style="color:#89DDFF;">:</span></span>
+<span class="line"><span style="color:#A6ACCD;">  </span><span style="color:#89DDFF;">-</span><span style="color:#A6ACCD;"> </span><span style="color:#C3E88D;">world</span></span>
+<span class="line"><span style="color:#A6ACCD;">  </span><span style="color:#89DDFF;">-</span><span style="color:#A6ACCD;"> </span><span style="color:#C3E88D;">world_nether</span></span>
+<span class="line"><span style="color:#A6ACCD;">  </span><span style="color:#89DDFF;">-</span><span style="color:#A6ACCD;"> </span><span style="color:#C3E88D;">world_the_end</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#676E95;"># \u5F00\u542F\u6B7B\u4EA1\u540E\u6267\u884C\u540E\u53F0\u6307\u4EE4\u7684\u4E16\u754C</span></span>
+<span class="line"><span style="color:#F07178;">worlds_enabled_punish_commands</span><span style="color:#89DDFF;">:</span></span>
+<span class="line"><span style="color:#A6ACCD;">  </span><span style="color:#89DDFF;">-</span><span style="color:#A6ACCD;"> </span><span style="color:#C3E88D;">world</span></span>
+<span class="line"><span style="color:#A6ACCD;">  </span><span style="color:#89DDFF;">-</span><span style="color:#A6ACCD;"> </span><span style="color:#C3E88D;">world_nether</span></span>
+<span class="line"><span style="color:#A6ACCD;">  </span><span style="color:#89DDFF;">-</span><span style="color:#A6ACCD;"> </span><span style="color:#C3E88D;">world_the_end</span></span>
+<span class="line"></span></code></pre></div><p>\u6309\u6CE8\u91CA\u4FEE\u6539\u914D\u7F6E\u5373\u53EF</p>`,9),c=[e];function t(r,y,i,D,C,d){return n(),a("div",null,c)}const A=s(o,[["render",t]]);export{_ as __pageData,A as default};
